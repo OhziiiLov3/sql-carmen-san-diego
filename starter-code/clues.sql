d3132b8a66b168c79d03c699ecb3f13e02f60407
ohziiilov3 <klbaskerville0520@gmail.com>
2022-08-13 15:14:36 -0700

    adding notes

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -2,12 +2,22 @@
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 
-
+SELECT name, population 
+FROM countries 
+WHERE region = 'Southern Europe' 
+ORDER BY population DESC;
+--  Answer: Holy See (Vatican Cities State) = 1000 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
+
+SELECT *
+FROM countrylanguages 
+WHERE countrycode = 'VAT';
+
+-- ANSWER: ITALIAN
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
@@ -15,12 +25,18 @@
 --  nearby country speaks nothing but that language.
 -- Hint: first find the countries where only one language is spoken. Then, in a separate query, find the countries where Italian is spoken. Then cross reference these lists by hand.
 
+SELECT * FROM countrylanguages WHERE language  = 'Italian'; 
+SELECT name FROM countries WHERE code = 'SMR' AND region = 'Southern Europe'; 
+
+-- ANSWER: SMR 
+-- ANSWER: San Marion 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
 
+SELECT * FROM countries 
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
